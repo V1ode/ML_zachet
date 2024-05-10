7a4f06787195a6b289b6c3f66955c1574bf00aaa
Makarov_Stanislav <makarov1003@mail.ru>
2024-05-10 09:40:27 +0300

    add lab4.html and f_lab1 code

--- flsite.py
+++ flsite.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 menu = [{"name": "Лаба 1", "url": "p_knn"},
         {"name": "Лаба 2", "url": "p_lab2"},
-        {"name": "Лаба 3", "url": "p_lab3"}]
+        {"name": "Лаба 3", "url": "p_lab3"},
+        {"name": "Лаба 4", "url": "p_lab4"}]
 
 iris_df=pd.read_csv("model/IRIS.csv")
 X=iris_df.drop(["species"],axis=1)
@@ -23,16 +24,15 @@ def index():
     return render_template('index.html', title="Лабораторные работы, выполненные Терешиным Р.П.", menu=menu)
 
 
+
 @app.route("/p_knn", methods=['POST', 'GET'])
 def f_lab1():
     if request.method == 'GET':
         return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu, class_model='')
     if request.method == 'POST':
         X_new = np.array([[float(request.form['list1']),
-                           float(request.form['list2']),
-                           float(request.form['list3']),
-                           float(request.form['list4'])]])
-        pred = model_knn.predict(X_new)
+                           float(request.form['list2'])]])
+        pred = model_knn.predict(X_new)           # Нужна модель
         return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu,
                                class_model="Это: " + pred)
 
@@ -43,7 +43,11 @@ def f_lab2():
 
 @app.route("/p_lab3")
 def f_lab3():
-    return render_template('lab3.html', title="Логистическая регрессия", menu=menu)
+    return render_template('lab3.html', title="Линейная регрессия", menu=menu)
+
+@app.route("/p_lab4")
+def f_lab4():
+    return render_template('lab4.html', title="Дерево решений", menu=menu)
 
 
 if __name__ == "__main__":
